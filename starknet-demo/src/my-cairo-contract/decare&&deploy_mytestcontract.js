24445faeee1e0613e9c780204060be25e8d3edcc
zhengpeng <847850277@qq.com>
2024-04-22 16:19:27 +0800

    feat: 部分枚举类型的代码

--- starknet-demo/src/my-cairo-contract/decare&&deploy_mytestcontract.js
+++ starknet-demo/src/my-cairo-contract/decare&&deploy_mytestcontract.js
@@ -7,8 +7,8 @@ async function main() {
     const provider = new RpcProvider({ nodeUrl: "http://127.0.0.1:5050/rpc" }); // only for starknet-devnet-rs
 
     //连接已经存在的账户
-    const privateKey0 = "0x2cf91d0b929f32b337d44736dc57e495";
-    const address0 = "0x4b7d08d00133d3ff9df1b4f3019fd7981f3a40f210f16d1080581f0a685e77a";
+    const privateKey0 = "0xf9f1167dfa384de9be7a57f7bbd29c11";
+    const address0 = "0x1a34b4767795562e1fef04a6868b62d0bf671ef9466a744548e37c5076889a8";
     const account0 = new Account(provider, address0, privateKey0);
 
     // Declare & deploy Test contract in devnet
@@ -32,15 +32,28 @@ async function main() {
     // Interactions with the contract with call & invoke
     myTestContract.connect(account0);
 
-    const response1 = await myTestContract.test2(10);
-
+    const response1 = await myTestContract.increase_counter(10);
     console.log("response1",response1);
-
-    console.log("response1",response1.unwrap(),response1.activeVariant())
-
-    const response2 = await myTestContract.test2(100);
-
+    const response2 = await myTestContract.decrease_counter(11);
     console.log("response2",response2);
+
+    const response3 = await myTestContract.test1(1);
+    console.log("response3",response3);
+
+    const response4 = await myTestContract.test2(100);
+    console.log("response4",response4);
+
+    const response5 = await myTestContract.call("test2",[200]);
+    console.log("response5",response5);
+
+    const response6 = await myTestContract.call("test3",[101]);
+    console.log("response6",response6);
+
+    const response7 = await myTestContract.call("test3",[10]);
+    console.log("response7",response7);
+
+
+
 
 
 
